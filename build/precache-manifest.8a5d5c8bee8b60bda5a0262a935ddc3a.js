f9846210c4b976529f8cd567a439498e759611d6
Sérgio Júnior <sergioamjr91@gmail.com>
2020-12-12 12:04:11 -0300

    Built project

---
+++ build/precache-manifest.8a5d5c8bee8b60bda5a0262a935ddc3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3195fc185e9248ea77dac3499b86e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28c0bb2d3fbe3ab25c00",
+    "url": "/static/css/main.f6a0bb11.chunk.css"
+  },
+  {
+    "revision": "6f67ca8c4eee609ce75d",
+    "url": "/static/js/2.7ee3358d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ee3358d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c0bb2d3fbe3ab25c00",
+    "url": "/static/js/main.1fc2b0d0.chunk.js"
+  },
+  {
+    "revision": "0bb3bc05f2f650572cf9",
+    "url": "/static/js/runtime-main.846859eb.js"
+  }
+]);
